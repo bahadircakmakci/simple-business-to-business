71f6232c431f08199c288bbb76555eefbfaeba93
bahadir <bahadircakmakci53@users.noreply.github.com>
2021-03-19 21:08:00 +0300

    Domain Layer entityler Oluşturuluyor

---
+++ simple-business-to-business.DomainLayer/Entities/Concrete/Companies.cs
@@ -0,0 +1,37 @@
+﻿using simple_business_to_business.DomainLayer.Entities.Interfaces;
+using simple_business_to_business.DomainLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simple_business_to_business.DomainLayer.Entities.Concrete
+{
+    public class Companies : IBaseEntity
+    {
+
+        public int Id { get; set; }
+
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+        public string Fax { get; set; }
+        public string PlasiyerId { get; set; }
+
+
+
+        public decimal TotalBalance  { get; set; }
+
+
+        private DateTime _createDate = DateTime.Now;
+        public DateTime CreateDate { get => _createDate; set => _createDate = value; }
+
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+        private Status _status = Status.Active;
+        public Status Status { get => _status; set => _status = value; }
+    }
+}
---
+++ simple-business-to-business.DomainLayer/Entities/Interfaces/IBaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using simple_business_to_business.DomainLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simple_business_to_business.DomainLayer.Entities.Interfaces
+{
+    public interface IBaseEntity
+    {
+        DateTime CreateDate { get; set; }
+        DateTime? UpdateDate { get; set; }
+        DateTime? DeleteDate { get; set; }
+        Status Status { get; set; }
+    }
+}
